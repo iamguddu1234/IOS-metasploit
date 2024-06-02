170b27ee7c8b38c6187734efcfd2a5067c21f6c9
Akshay Bhamse <iamguddu37@gmail.com>
2024-06-02 15:55:34 +0530

    Initial Commit

---
+++ Metasploit/MetasploitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetasploitApp.swift
+//  Metasploit
+//
+//  Created by Akshay Bhasme on 02/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MetasploitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
